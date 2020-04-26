bc34204ab7de85a7e0b4c980eb79488448758d65
Jesse <jasanford24@gmail.com>
2020-04-25 20:37:32 -0400

    final updates

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # corona
 Corona Virus SMS System for my Raspberry Pi 4B
 
-Checks 'https://coronavirus.1point3acres.com/en' for Covid-19 data every day at 8pmEST and\
+Checks 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv' for Covid-19 data and\
 sends out a text to a list of numbers.
 
 
 Requires Twilio account and additional files:
 
-<pre><code>corona-database.db    -   Database with cases and accounts tables.</code></pre>
+<pre><code>corona-database.db    -   Database with accounts tables.</code></pre>
--- corona.py
+++ corona.py
@@ -16,35 +16,22 @@ logging.basicConfig(filename='corona.log',
                     format='%(asctime)s - %(message)s',
                     level=logging.INFO)
 
-
-EIGHT = 20 * 60 * 60
+TWO = 14 * 60 * 60
 DAY = 24 * 60 * 60
+
 URL = ('https://raw.githubusercontent.com/nytimes/'
        'covid-19-data/master/us-counties.csv')
 
 
-def calculate_time(seven=False):
+def calculate_time():
     hour = (60 * 60) * localtime()[3]
     minute = 60 * localtime()[4]
     second = localtime()[5]
-    time_to_sleep = EIGHT - (hour + minute + second)
+    time_to_sleep = TWO - (hour + minute + second)
 
-    if time_to_sleep < 0 and seven:
+    if time_to_sleep < 0:
         return DAY + time_to_sleep
     return time_to_sleep
-
-
-def to_sleep(seven=False):
-    if seven:
-        logging.info('Sleeping until 7:58pmEST.')
-        seven = calculate_time(True) - 120
-        if seven > 0:
-            sleep(seven)
-    else:
-        logging.info('Sleeping until 8:00pmEST.')
-        eight = calculate_time()
-        if eight > 0:
-            sleep(eight)
 
 
 def collect_data():
@@ -59,42 +46,39 @@ def collect_data():
         sleep(3600 - localtime()[5])
         collect_data()
     logging.info('Data collected.')
-    return yesterdata, prior
+    return yesterdata, prior, df
 
 
-def main_db_parse(test=True):
-    logging.info('Starting database engine.')
+def main(test=True):
+    if not test:
+        sleep(calculate_time())
+
+    data, prior, full = collect_data()
+
     engine = create_engine('sqlite:///corona-database.db')
+    full.to_sql(name='cases', if_exists='replace', con=engine, index=False)
+
     with engine.connect() as connection:
         accounts = tuple(connection.execute('SELECT * FROM Accounts'))
-
-    logging.info('Database engine closed.')
     engine.dispose()
 
-    data, prior = collect_data()
-
     # Loops through accounts one by one storing relevant data.
-    logging.info('Beginning creation of individual data.')
+    logging.info('Creating individual data.')
     for account in accounts:
         recipient = Account(*account)
         recipient.set_data(data, prior)
         logging.info('Sending message to: ' + recipient.number)
         print(recipient) if test else recipient.send_sms()
 
-    # Closes database connection.
     if not test:
         main(test=False)
 
 
-def main(test=True):
-    main_db_parse() if test else main_db_parse(test=False)
-
-
 if __name__ == '__main__':
-    try:
+    if len(sys.argv) > 1:
         if sys.argv[1].lower() == "run":
             main(test=False)
         else:
             main()
-    except IndexError:
+    else:
         main()
